351d2bddd9f0695d49e32ec413c9c0741fc14fb4
cyb880326 <693340562@qq.com>
2021-08-14 22:09:41 +0800

    Create boot.sh

---
+++ boot.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+
+basedir=$(cd "$(dirname "$0")"; pwd)
+. ./venv/bin/activate
+
+function getpid() {
+  pid=`pstree -ap|grep gunicorn| grep -v grep| head -n 1| awk -F ',' '{print $2}'| awk '{print $1}'`
+}
+
+
+function start(){
+    getpid;
+    if [[ ! -z $pid ]];then
+        echo "服务已运行中,pid:" $pid
+        exit 1;
+    fi
+    cpu=`cat /proc/cpuinfo| grep "processor"| wc -l`
+    ts=`expr $cpu \* 2 + 1`
+    
+    gunicorn -D -w $ts -b 0.0.0.0:8000 app:app
+}
+
+function stop(){
+    getpid;
+    if [[ ! -z $pid ]];then
+        echo "服务已运行中,pid:" $pid
+    else
+      echo "服务未启动,无需停止."
+      exit 1;
+    fi
+    kill -9 $pid
+}
+
+function restart(){
+    getpid;
+    if [[ ! -z $pid ]];then
+        echo "服务已运行中,pid:" $pid
+    else
+      echo "服务未启动,无需停止."
+      start;
+      exit 1;
+    fi
+    kill -HUP $pid
+}
+
+function status(){
+    getpid;
+    if [[ ! -z $pid ]];then
+        echo "服务运行中,pid:" $pid
+    else
+        echo "服务未启动."
+        exit 1;
+    fi
+}
+
+function usage(){
+    echo "$0 <start|stop|restart|status>"
+}
+
+case $1 in
+    start)
+       start;
+       ;;
+    stop)
+       stop;
+       ;;
+    restart)
+       restart;
+       ;;
+    status)
+        status;
+        ;;
+    *)
+       usage;
+       ;;
+esac
