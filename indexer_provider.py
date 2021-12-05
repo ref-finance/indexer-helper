b3768b82b8deae2c550ff400ce360f6f38235b81
Marco <sun.dsk1@gmail.com>
2021-12-05 20:26:13 +0800

    add ft_transfer_call and wnear to user activities

--- indexer_provider.py
+++ indexer_provider.py
@@ -63,7 +63,7 @@ def get_actions(network_id, account_id):
         "from action_receipt_actions join receipts using(receipt_id) "
         "join execution_outcomes using(receipt_id) " 
     )
-    sql2 = "where (action_kind = 'FUNCTION_CALL' and (receiver_account_id = '%s' or receiver_account_id = '%s') " % (Cfg.NETWORK[network_id]["REF_CONTRACT"], Cfg.NETWORK[network_id]["FARMING_CONTRACT"])
+    sql2 = "where (action_kind = 'FUNCTION_CALL' and (receiver_account_id in ('%s', '%s', 'wrap.near') or args->'args_json'->>'receiver_id' = '%s') " % (Cfg.NETWORK[network_id]["REF_CONTRACT"], Cfg.NETWORK[network_id]["FARMING_CONTRACT"], Cfg.NETWORK[network_id]["REF_CONTRACT"])
     sql3 = """and predecessor_account_id = %s ) order by timestamp desc limit 10""" 
     sql = "%s %s %s" % (sql1, sql2, sql3)
 
