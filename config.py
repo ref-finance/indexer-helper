cff2a5d1213fe98086e1a32aba782f0a3cf531b1
dom-jiang <38778185+dom-jiang@users.noreply.github.com>
2023-05-31 16:02:17 +0800

    add burrow contract (#55)
    * pgsql replace
    
    * add white list token price
    
    * add white list token price
    
    * add white list token price
    
    * add white list token price
    
    * add token price report
    
    * pgsql replace
    
    * add token price ratio report
    
    * add token price ratio report
    
    * add token price ratio report
    
    * add token price ratio report
    
    * add token price ratio report
    
    * add burrow contract
    
    ---------
    
    Co-authored-by: dom-jiang <805816775@.qq.com>

--- config.py
+++ config.py
@@ -123,6 +123,7 @@ class Cfg:
             "DCL_POOL_CONTRACT": "dclv2.ref-labs.near",
             "DCL_CONTRACT": "dclv2.ref-labs.near",
             "ORDERLY_CONTRACT": "asset-manager.orderly-network.near",
+            "BURROW_CONTRACT": "contract.main.burrow.near",
             "REDIS_KEY": "FARMS_MAINNET",
             "REDIS_POOL_BY_TOKEN_KEY": "POOLS_BY_TOKEN_MAINNET",
             "REDIS_POOL_KEY": "POOLS_MAINNET",
