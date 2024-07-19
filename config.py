7198d8246871cfdd916dfb0200b93a024c8a5322
dom-jiang <805816775@.qq.com>
2024-07-19 15:54:09 +0800

    Add usdc token price

--- config.py
+++ config.py
@@ -323,6 +323,7 @@ class Cfg:
             {"SYMBOL": "FAST", "NEAR_ID": "edge-fast.near", "MD_ID": "v2.ref-finance.near|5167|wrap.near", "DECIMAL": 24},
             {"SYMBOL": "HAT", "NEAR_ID": "hat.tkn.near", "MD_ID": "v2.ref-finance.near|5179|wrap.near", "DECIMAL": 18},
             {"SYMBOL": "INTEL", "NEAR_ID": "intel.tkn.near", "MD_ID": "v2.ref-finance.near|4663|wrap.near", "DECIMAL": 18},
+            {"SYMBOL": "USDC", "NEAR_ID": "16.contract.portalbridge.near", "MD_ID": "usd-coin", "DECIMAL": 6},
         ],
         "BASE_MAINNET": [
             {"SYMBOL": "cbETH", "MD_ID": "coinbase-wrapped-staked-eth"},
