3fcfd28d70772f59707ce43b32c03ee79090d5de
dom-jiang <805816775@.qq.com>
2024-08-01 22:35:51 +0800

    Add dd&neiro token price

--- config.py
+++ config.py
@@ -342,6 +342,8 @@ class Cfg:
             {"SYMBOL": "HAT", "NEAR_ID": "hat.tkn.near", "MD_ID": "v2.ref-finance.near|5179|wrap.near", "DECIMAL": 18},
             {"SYMBOL": "INTEL", "NEAR_ID": "intel.tkn.near", "MD_ID": "v2.ref-finance.near|4663|wrap.near", "DECIMAL": 18},
             {"SYMBOL": "USDC", "NEAR_ID": "16.contract.portalbridge.near", "MD_ID": "usd-coin", "DECIMAL": 6},
+            {"SYMBOL": "DD", "NEAR_ID": "dd.tg", "MD_ID": "v2.ref-finance.near|5222|wrap.near", "DECIMAL": 8},
+            {"SYMBOL": "NEIRO", "NEAR_ID": "neiro.token0.near", "MD_ID": "v2.ref-finance.near|5255|wrap.near", "DECIMAL": 18},
         ],
         "BASE_MAINNET": [
             {"SYMBOL": "cbETH", "MD_ID": "coinbase-wrapped-staked-eth"},
