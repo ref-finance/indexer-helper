fbcb208a2f6b8a6450221508b6f173db3350e67c
dom-jiang <805816775@.qq.com>
2024-09-16 18:00:20 +0800

    Add KAT token price

--- config.py
+++ config.py
@@ -409,6 +409,7 @@ class Cfg:
             {"SYMBOL": "USDC", "NEAR_ID": "16.contract.portalbridge.near",
              "MD_ID": "v2.ref-finance.near|NA|17208628f84f5d6ad33f0da3bbbeb27ffcb398eac501a31bd6ad2011e36133a1",
              "DECIMAL": 6},
+            {"SYMBOL": "KAT", "NEAR_ID": "kat.token0.near", "MD_ID": "v2.ref-finance.near|5391|wrap.near", "DECIMAL": 18},
         ],
         "BASE_MAINNET": [
             {"SYMBOL": "cbETH", "MD_ID": "coinbase-wrapped-staked-eth"},
